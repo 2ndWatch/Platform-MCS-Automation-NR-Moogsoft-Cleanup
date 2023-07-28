59d328263e26a92e3e39100a23f9ba5fbbc8403f
Alex <aeversmeyer@2ndwatch.com>
2023-07-28 12:21:36 -0600

    add nrql for creating channels and workflows

--- create_catchall.py
+++ create_catchall.py
@@ -1,82 +1,17 @@
 from string import Template
 import requests
+import json
 
 
-def create_channel(destination_id, account_id, logger):
-    # key = 'payload'
-    channel_payload = """
-        {\n\"alert_level\": \"{{#if issueClosedAt}}closed{{else if 
-        issueAcknowledgedAt}}acknowledged{{else}}open{{/if}}-{{escape accumulations.conditionName.[0]}}\",
-        \n\"current_state\": \"{{#if issueClosedAt}}closed{{else if issueAcknowledgedAt}}acknowledged{{else}}open{{
-        /if}}\",\n\"description\": {\n\"account_id\": {{nrAccountId}},\n\"account_name\": \"{{accumulations.tag.account.[
-        0]}}\",\n\"condition_id\": {{accumulations.conditionFamilyId.[0]}},\n\"condition_name\": \"{{escape 
-        accumulations.conditionName.[0]}}\",\n\"current_state\": \"{{#if issueClosedAt}}closed{{else if 
-        issueAcknowledgedAt}}acknowledged{{else}}open{{/if}}\",\n\"details\": \"{{escape issueTitle}}\",\n\"event_type\": 
-        \"INCIDENT\",\n\"incident_acknowledge_url\": \"<a href={{issuePageUrl}}>{{issuePageUrl}}</a>\",\n\"incident_id\": 
-        {{#if labels.nrIncidentId}}{{labels.nrIncidentId.[0]}}{{else}}-1{{/if}},\n\"incident_url\": \"{{issuePageUrl}}\",
-        \n\"owner\": \"{{owner}}\",\n\"policy_name\": \"{{escape accumulations.policyName.[0]}}\",\n\"policy_url\": \"{{
-        policyUrl}}\",\n\"runbook_url\": \"<a href={{accumulations.runbookUrl.[0]}}>{{accumulations.runbookUrl.[
-        0]}}</a>\",\n\"severity\": \"{{#eq 'HIGH' priority}}WARNING{{else}}{{priority}}{{/eq}}\",\n\"timestamp\": {{
-        updatedAt}}\n  },\n\"details\": \"{{escape issueTitle}}\",\n\"metric\": \"{{escape accumulations.policyName.[
-        0]}}\",\n\"severity\": \"{{#eq 'HIGH' priority}}WARNING{{else}}{{priority}}{{/eq}}\",\n\"targets\": [\n { 
-        \n\"id\": \"{{labels.targetId.[0]}}\",\n\"name\": \"{{#if accumulations.targetName}}{{escape 
-        accumulations.targetName.[0]}}{{else if entitiesData.entities}}{{escape entitiesData.entities.[0].name}}{{
-        else}}N/A{{/if}}\",\n\"link\": \"{{issuePageUrl}}\",\n\"product\": \"{{accumulations.conditionProduct.[0]}}\",
-        \n\"type\": \"{{#if entitiesData.types.[0]}}{{entitiesData.types.[0]}}{{else}}N/A{{/if}}\",\n\"labels\": { {{
-        #each accumulations.rawTag}}\"{{escape @key}}\": {{#if this.[0]}}{{json this.[0]}}{{else}}\"empty\"{{/if}}{{
-        #unless @last}},{{/unless}}{{/each}}\n } \n} \n ],\n\"timestamp\": {{updatedAt}}\n}
-        """
+def get_destination_id(endpoint, headers, account_id, logger):
+    destination_id = ''
 
-    nr_gql_create_channel = Template("""
-        mutation {
-          aiNotificationsCreateChannel(accountId: $account_id, channel: {
-            type: WEBHOOK,
-            name: "2W Platform API",
-            destinationId: $destination_id,
-            product: IITL,
-            properties: [
-              {
-              key:"payload",
-              value: "$payload"
-              },
-              {
-              key:"headers",
-              value:"{\"x-api-key\":\"09b99595-8aef-45f7-99af-54e823a59895\"}"
-              }
-            ]
-          }) {
-            channel {
-              id
-              name
-            }
-          }
-        }
-        """)
-
-    workflow_query_fmtd = nr_gql_create_channel.substitute({'account_id': account_id})
-    nr_response = requests.post(nr_endpoint,
-                                headers=nr_headers,
-                                json={'query': workflow_query_fmtd}).json()
-
-    return
-
-
-def create_catchall_workflow(client_name, account_id, logger):
-
-    # TODO: get all workflow IDs from an account and find the ID for '2W Platform API'
-    platform_destination_id = ''
-    nr_endpoint = 'https://api.newrelic.com/graphql'
-    nr_headers = {
-        'Content-Type': 'application/json',
-        'API-Key': 'NRAK-7DVT82DILPFIAXSZZ6CLPKYB8YU',
-    }
-
-    nr_gql_destinations_query = Template("""
+    nrql_destinations_query = Template("""
     {
       actor {
         account(id: $account_id) {
           aiNotifications {
-            destinations(filters: {name: "2W Platform API"}) {
+            destinations {
               entities {
                 id
                 name
@@ -88,20 +23,186 @@ def create_catchall_workflow(client_name, account_id, logger):
     }
     """)
 
-    destinations_query_fmtd = nr_gql_destinations_query.substitute({'account_id': account_id})
-    nr_response = requests.post(nr_endpoint,
-                                headers=nr_headers,
+    destinations_query_fmtd = nrql_destinations_query.substitute({'account_id': account_id})
+    nr_response = requests.post(endpoint,
+                                headers=headers,
                                 json={'query': destinations_query_fmtd}).json()
 
     destinations_list = nr_response['data']['actor']['account']['aiNotifications']['destinations']['entities']
-    if destinations_list:
-        platform_destination_id = destinations_list[0]['id']
 
-    # TODO: get all policy IDs that currently use the '2W Platform API' webhook in a workflow except any named
-    #  'Platform Catchall'
+    if destinations_list:
+        for destination in destinations_list:
+            if 'Platform' in destination['name']:
+                destination_id = destination['id']
+
+    return destination_id
+
+
+def get_policy_workflow_ids():
+
+    # response['data']['actor']['account']['aiWorkflows']['workflows']['entities'] (list of workflow entities)
+    nrql_workflows_query = Template("""
+    {
+      actor {
+        user {
+          name
+        }
+        account(id: 2621186) {
+          aiWorkflows {
+            workflows {
+              entities {
+                id
+                name
+                destinationConfigurations {
+                  name
+                  type
+                  notificationTriggers
+                }
+                issuesFilter {
+                  predicates {
+                    attribute
+                    operator
+                    values
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    """)
+
+    return
+
+
+def create_channel(endpoint, headers, destination_id, account_id, logger):
+    channel_id = ''
+
+    api_key = json.dumps("{\"x-api-key\":\"09b99595-8aef-45f7-99af-54e823a59895\"}")
+    destination_id = json.dumps(destination_id)
+    payload = json.dumps("{\n\"alert_level\": \"{{#if issueClosedAt}}closed{{else if issueAcknowledgedAt}}acknowledged{{else}}open{{/if}}-{{escape accumulations.conditionName.[0]}}\",\n\"current_state\": \"{{#if issueClosedAt}}closed{{else if issueAcknowledgedAt}}acknowledged{{else}}open{{/if}}\",\n\"description\": {\n\"account_id\": {{nrAccountId}},\n\"account_name\": \"{{accumulations.tag.account.[0]}}\",\n\"condition_id\": {{accumulations.conditionFamilyId.[0]}},\n\"condition_name\": \"{{escape accumulations.conditionName.[0]}}\",\n\"current_state\": \"{{#if issueClosedAt}}closed{{else if issueAcknowledgedAt}}acknowledged{{else}}open{{/if}}\",\n\"details\": \"{{escape issueTitle}}\",\n\"event_type\": \"INCIDENT\",\n\"incident_acknowledge_url\": \"<a href={{issuePageUrl}}>{{issuePageUrl}}</a>\",\n\"incident_id\": {{#if labels.nrIncidentId}}{{labels.nrIncidentId.[0]}}{{else}}-1{{/if}},\n\"incident_url\": \"{{issuePageUrl}}\",\n\"owner\": \"{{owner}}\",\n\"policy_name\": \"{{escape accumulations.policyName.[0]}}\",\n\"policy_url\": \"{{policyUrl}}\",\n\"runbook_url\": \"<a href={{accumulations.runbookUrl.[0]}}>{{accumulations.runbookUrl.[0]}}</a>\",\n\"severity\": \"{{#eq 'HIGH' priority}}WARNING{{else}}{{priority}}{{/eq}}\",\n\"timestamp\": {{updatedAt}}\n  },\n\"details\": \"{{escape issueTitle}}\",\n\"metric\": \"{{escape accumulations.policyName.[0]}}\",\n\"severity\": \"{{#eq 'HIGH' priority}}WARNING{{else}}{{priority}}{{/eq}}\",\n\"targets\": [\n { \n\"id\": \"{{labels.targetId.[0]}}\",\n\"name\": \"{{#if accumulations.targetName}}{{escape accumulations.targetName.[0]}}{{else if entitiesData.entities}}{{escape entitiesData.entities.[0].name}}{{else}}N/A{{/if}}\",\n\"link\": \"{{issuePageUrl}}\",\n\"product\": \"{{accumulations.conditionProduct.[0]}}\",\n\"type\": \"{{#if entitiesData.types.[0]}}{{entitiesData.types.[0]}}{{else}}N/A{{/if}}\",\n\"labels\": { {{#each accumulations.rawTag}}\"{{escape @key}}\": {{#if this.[0]}}{{json this.[0]}}{{else}}\"empty\"{{/if}}{{#unless @last}},{{/unless}}{{/each}}\n } \n} \n ],\n\"timestamp\": {{updatedAt}}\n}")
+
+    nrql_create_channel = Template("""
+        mutation {
+          aiNotificationsCreateChannel(accountId: $account_id, channel: {
+            type: WEBHOOK,
+            name: "alex actual final test",
+            destinationId: $destination_id,
+            product: IINT,
+            properties: [
+              {
+              key:"payload",
+              value: $payload
+              },
+              {
+              key:"headers",
+              value: $api_key
+              }
+            ]
+          }) {
+            channel {
+              id
+              name
+            }
+          }
+        }
+        """)
+
+    create_channel_fmtd = nrql_create_channel.substitute({'account_id': account_id,
+                                                          'destination_id': destination_id,
+                                                          'payload': payload,
+                                                          'api_key': api_key})
+    nr_response = requests.post(endpoint,
+                                headers=headers,
+                                json={'query': create_channel_fmtd}).json()
+    try:
+        channel_id = nr_response['data']['aiNotificationsCreateChannel']['channel']['id']
+        logger.info(f'Platform API orphan channel ID: {channel_id}')
+    except KeyError:
+        logger.warning(nr_response)
+
+    return channel_id
+
+
+def create_workflow(endpoint, headers, account_id, channel_id, logger):
+    workflow_id = ''
+
+    # TODO: add policy_ids to parameters
+
+    channel_id = json.dumps(channel_id)
+    test_policy_ids = json.dumps(['4569885'])
+
+    nrql_create_workflow = Template("""
+    mutation {
+      aiWorkflowsCreateWorkflow(
+        accountId: $account_id
+        createWorkflowData: {
+          destinationConfigurations: {
+            channelId: $channel_id, 
+            notificationTriggers: [ACTIVATED, ACKNOWLEDGED, CLOSED]
+          }, 
+          mutingRulesHandling: DONT_NOTIFY_FULLY_MUTED_ISSUES, 
+          name: "alex test 3", 
+          workflowEnabled: true, 
+          destinationsEnabled: true,
+          issuesFilter: {
+            type: FILTER,
+            predicates: [
+              {
+                attribute: "labels.policyIds",
+                operator: EXACTLY_MATCHES,
+                values: $policy_ids_list
+              }
+            ]
+          }
+        }
+      ) {
+        workflow {
+          id
+          name
+        }
+      }
+    }
+    """)
+
+    create_workflow_fmtd = nrql_create_workflow.substitute({'account_id': account_id,
+                                                            'channel_id': channel_id,
+                                                            'policy_ids_list': test_policy_ids})
+    nr_response = requests.post(endpoint,
+                                headers=headers,
+                                json={'query': create_workflow_fmtd}).json()
+
+    try:
+        workflow_id = nr_response['data']['aiWorkflowsCreateWorkflow']['workflow']['id']
+        logger.info(f'Platform API workflow ID: {workflow_id}')
+    except KeyError:
+        logger.warning(nr_response)
+
+    return workflow_id
+
+
+def create_catchall_workflow(client_name, account_id, logger):
+    endpoint = 'https://api.newrelic.com/graphql'
+    headers = {
+        'Content-Type': 'application/json',
+        'API-Key': 'NRAK-7DVT82DILPFIAXSZZ6CLPKYB8YU',
+    }
+
+    # get all destination IDs from an account and find the ID for '2W Platform API'
+    destination_id = get_destination_id(endpoint, headers, account_id, logger)
+    logger.info(f'Platform API destination ID: {destination_id}')
 
     # TODO: create a new channel for '2W Platform API' destination
+    channel_id = create_channel(endpoint, headers, destination_id, account_id, logger)
 
-    channel_id = create_channel(platform_destination_id, account_id, logger)
+    # TODO: create a new workflow called 'MCS Platform' & associate policies
+    workflow_id = create_workflow(endpoint, headers, account_id, channel_id, logger)
+
+    # TODO: get all workflows that currently use the '2W Platform API' webhook except any with 'Platform' in the name;
+    #   return a list of policy IDs & list of workflow IDs
+
+    # TODO: disable appropriate policies
+    # pass in workflow IDs
 
     return 0
--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def initialize_logger():
     return logger
 
 
-def nr_function(logger):
+def get_nr_account_ids(logger):
     nr_endpoint = 'https://api.newrelic.com/graphql'
     nr_headers = {
         'Content-Type': 'application/json',
@@ -38,86 +38,6 @@ def nr_function(logger):
     }
     """)
 
-    # response['data']['actor']['account']['aiNotifications']['destinations'] (list of destinations)
-    # destination keys: 'id', 'name'
-    # 'id' if 'Moogsoft' (case-insensitive) in 'name'
-    nr_gql_destinations_query = Template("""
-    {
-      actor {
-        account(id: $account_id) {
-          aiNotifications {
-            destinations(filters: {type: WEBHOOK}) {
-              entities {
-                name
-                id
-              }
-            }
-          }
-        }
-      }
-    }
-    """)
-
-    # response['data']['actor']['account']['alerts']['policiesSearch']['policies'] (list of policies)
-    # policy keys: 'id', 'name'
-    nr_gql_policies_query = Template("""
-    {
-      actor {
-        account(id: 2621186) {
-          alerts {
-            policiesSearch {
-              policies {
-                id
-                name
-              }
-            }
-          }
-        }
-      }
-    }
-    """)
-
-    # response['data']['actor']['account']['aiWorkflows']['workflows']['entities'] (list of workflow entities)
-    # entity keys: 'id', 'name', 'workflowEnabled', 'destinationConfigurations'
-    # destinationConfigurations keys: 'channelId', 'name', 'type'
-    nr_gql_workflows_query = Template("""
-    {
-      actor {
-        account(id: 2621186) {
-          aiWorkflows {
-            workflows {
-              entities {
-                id
-                name
-                workflowEnabled
-                lastRun
-                destinationConfigurations {
-                  channelId
-                  name
-                  type
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-    """)
-
-    # TODO: get list of account numbers in NR
-    # TODO: for each account, get list of policies
-    # TODO: for each account, get list of workflows
-    # TODO: for each workflow, if it has a 2W Platform destination, collect policy name (should be in workflow name)
-    #  and id
-    # TODO: create catchall workflow with 2W Platform destination that includes all policies frm list in previous step
-    # TODO: disable all individual workflows with 2W Platform destination
-
-    # --- end automation for now ---
-
-    # TODO: will need to manually go in and remove Moogsoft destination from any still-active workflows
-    # TODO: if disabling individual workflows has no impact on monitoring, then go back and delete those workflows
-    # TODO: once Moogsoft destinations are no longer used by any workflows, the destinations can be deleted
-
     accounts_query_fmtd = nr_gql_accounts_query.substitute({})
     nr_response = requests.post(nr_endpoint,
                                 headers=nr_headers,
@@ -130,26 +50,29 @@ def nr_function(logger):
 def do_the_things():
     logger = initialize_logger()
 
-    # accounts = nr_function(logger)
+    # generate an Excel report of all NR workflows
+    # accounts = get_nr_account_ids(logger)
+    # for account in accounts['data']['actor']['accounts']:
+    #     client_name = account['name']
+    #     account_id = account['id']
+    #     logger.info(f'{client_name}: {account_id}')
+    #
+    #     client_name_sliced = client_name[:30]
+    #
+    #     process_result = wr.generate_workflow_report(client_name_sliced, account_id, logger)
+    #
+    #     if process_result == 0:
+    #         logger.info(f'\n{client_name} processed successfully.\n')
+    #
+    # workbook = openpyxl.load_workbook('Workflow Report.xlsx')
+    # workbook._sheets.sort(key=lambda ws: ws.title)
+    # workbook.save('Workflow Report sorted.xlsx')
 
-    for account in accounts['data']['actor']['accounts']:
-        client_name = account['name']
-        account_id = account['id']
-        logger.info(f'{client_name}: {account_id}')
-
-        client_name_sliced = client_name[:30]
-
-        process_result = wr.generate_workflow_report(client_name_sliced, account_id, logger)
-
-        if process_result == 0:
-            logger.info(f'\n{client_name} processed successfully.\n')
-
-    workbook = openpyxl.load_workbook('Workflow Report.xlsx')
-    workbook._sheets.sort(key=lambda ws: ws.title)
-    workbook.save('Workflow Report sorted.xlsx')
+    # 3720977 2W-MCS-Tooling-Test
+    # Test policy: 4569885
 
     # 2621186 2W-MCS-2ndWatch
-    # process_result = wr.generate_workflow_report('2W-MCS-2ndWatch', 2621186, logger)
+    process_result = cc.create_catchall_workflow('2W-MCS-Tooling-Test', 3720977, logger)
 
 
 do_the_things()
